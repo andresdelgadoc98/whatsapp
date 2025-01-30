e26deb4322dfe1c8bcb705ee72a7b2a69462f5fb
Andres Delgado <andresdelgadoc98@gmail.com>
2025-01-30 11:07:17 -0600

    jajja

--- src/routes/whatsapp.py
+++ src/routes/whatsapp.py
@@ -3,8 +3,8 @@ from flask import request, Blueprint,jsonify
 main = Blueprint('whatsapp', __name__)
 
 @main.route('/', methods=['POST'])
-def recibe_whatsapp():
-    data = request.form  # Twilio envía los datos en request.form
-    mensaje = data.get('Body', 'No message received')  # Obtiene el mensaje del usuario
-    print(f"Mensaje recibido: {mensaje}")  # Imprime en la consola
-    return "Mensaje recibido"  # Respuesta al webhook de Twilio
+def reply():
+    message = request.form.get('Body').lower()
+    print(message)
+    if message:
+        return f'Thank you for your message! A member of our team will be in touch with you soon.'
