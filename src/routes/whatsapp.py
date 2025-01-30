bc6010985b56e7ae1aa7c9819d3553c91d8264c4
Andres Delgado <andresdelgadoc98@gmail.com>
2025-01-30 11:15:37 -0600

    rps

--- src/routes/whatsapp.py
+++ src/routes/whatsapp.py
@@ -1,10 +1,16 @@
 from flask import request, Blueprint,jsonify
+from twilio.twiml.messaging_response import MessagingResponse
 
 main = Blueprint('whatsapp', __name__)
 
+
+
 @main.route('/', methods=['POST'])
 def whatsapp():
-    message = request.form.get('Body').lower()
-    print(message)
-    if message:
-        return f'Thank you for your message! A member of our team will be in touch with you soon.'
+    """Respond to incoming calls with a simple text message."""
+    msg = request.form.get('Body')
+    print(msg)
+    resp = MessagingResponse()
+    resp.message("You said: {}".format(msg))
+
+    return str(resp)
